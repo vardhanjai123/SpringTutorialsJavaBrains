33300817aeeffa1d8d755293ced994d79c5a3ee7
vardhanjai123 <shekharjai07@gmail.com>
2020-07-23 01:16:30 +0530

    Used Applicationcontext instead of BeanFactory

--- src/com/jaivardhan/DrawingApp.java
+++ src/com/jaivardhan/DrawingApp.java
@@ -1,14 +1,12 @@
 package com.jaivardhan;
-
-
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
-        BeanFactory beanFactory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle= (Triangle) beanFactory.getBean("triangle");
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle= (Triangle) context.getBean("triangle");
         triangle.draw();
     }
 }
