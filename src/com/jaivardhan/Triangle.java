4b7766bf25f587d43ae39b21baf14f529bcbd5eb
vardhanjai123 <shekharjai07@gmail.com>
2020-07-23 01:53:49 +0530

    Used Constructor Injection.Learnt constructor-args to use in bean tag to do constructor injection

--- src/com/jaivardhan/Triangle.java
+++ src/com/jaivardhan/Triangle.java
@@ -3,18 +3,27 @@ package com.jaivardhan;
 public class Triangle {
 
     private String type;
+    private int height;
+
+    public Triangle(String type,int height) {
+        this.type = type;
+        this.height=height;
+    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public int getHeight() {
+        return height;
     }
+    //    public void setType(String type) {
+//        this.type = type;
+//    }
 
     public void draw()
     {
-        System.out.println("Type of Triangle is "+type);
+        System.out.println("Type of Triangle is "+getType()+" and its height is "+getHeight());
     }
 
 }
