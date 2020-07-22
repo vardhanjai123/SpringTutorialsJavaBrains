71d5a6cdb3c59083b97ac4d9b4cc152b588d467a
vardhanjai123 <shekharjai07@gmail.com>
2020-07-23 01:13:33 +0530

    Used XmlBeanFactory instead of ClassPathApplicationContext

--- src/com/jaivardhan/DrawingApp.java
+++ src/com/jaivardhan/DrawingApp.java
@@ -3,13 +3,11 @@ package com.jaivardhan;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
     public static void main(String[] args) {
-        BeanFactory beanFactory=new ClassPathXmlApplicationContext("spring.xml");
+        BeanFactory beanFactory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
         Triangle triangle= (Triangle) beanFactory.getBean("triangle");
         triangle.draw();
     }
