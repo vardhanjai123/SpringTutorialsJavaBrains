2de852814800ad03b781abaa7efad039f6c969dd
vardhanjai123 <shekharjai07@gmail.com>
2020-07-23 11:55:07 +0530

    Used Autowire byName for autowiring dependencies

--- src/com/jaivardhan/Triangle.java
+++ src/com/jaivardhan/Triangle.java
@@ -1,23 +1,43 @@
 package com.jaivardhan;
-import java.util.List;
 
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw()
     {
-        for(Point p:points)
-            System.out.println("Point A=("+p.getX()+","+p.getY()+")");
+            System.out.println("Point A=("+pointA.getX()+","+pointA.getY()+")");
+            System.out.println("Point B=("+pointB.getX()+","+pointB.getY()+")");
+            System.out.println("Point C=("+pointC.getX()+","+pointC.getY()+")");
+
+
     }
 
 }
